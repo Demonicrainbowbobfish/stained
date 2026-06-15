8ad0c43e2d054230c86a2b549a4d8ef244233dbc
Demonicrainbowbobfish <stinkybob937@gmail.com>
2026-06-14 23:56:12 -0400

    New stuff

---
+++ src/main/java/io/sparkycreepster/custom/StainedSwordMaterialorsmt.java
@@ -0,0 +1,11 @@
+package io.sparkycreepster.custom;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class StainedSwordMaterialorsmt extends SwordItem {
+
+    public StainedSwordMaterialorsmt(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
--- src/main/java/io/sparkycreepster/general/Items.java
+++ src/main/java/io/sparkycreepster/general/Items.java
@@ -1,7 +1,10 @@
 package io.sparkycreepster.general;
 
+import io.sparkycreepster.custom.StainedSwordMaterialorsmt;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,7 +16,12 @@ public class Items {
     public static final Item BLOOD_VIAL = registerItem("blood_vial",
             new Item(new FabricItemSettings()));
     public static final Item STAINED_SWORD = registerItem("stained_sword",
-            new Item(new FabricItemSettings()));
+            new StainedSwordMaterialorsmt(
+                    ToolMaterials.NETHERITE,
+                    10,
+                    -2.4F,
+                    new FabricItemSettings()
+            ));
 
     private static net.minecraft.item.Item registerItem(String name, net.minecraft.item.Item item) {
         return Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), item);
