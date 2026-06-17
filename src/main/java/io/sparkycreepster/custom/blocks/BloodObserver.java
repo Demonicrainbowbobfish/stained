1e7da3ae25b807cc174635a1e4df943e1471343d
Demonicrainbowbobfish <stinkybob937@gmail.com>
2026-06-17 16:34:13 -0400

    Skeleton code or smth

--- src/main/java/io/sparkycreepster/custom/blocks/BloodObserver.java
+++ src/main/java/io/sparkycreepster/custom/blocks/BloodObserver.java
@@ -2,10 +2,14 @@ package io.sparkycreepster.custom.blocks;
 
 import net.minecraft.block.Block;
 
+import java.util.Map;
+import java.util.UUID;
+
 public class BloodObserver extends Block {
     public BloodObserver(Settings settings) {
         super(settings);
     }
+    private int radius = 64;
 
 
 }
---
+++ src/main/java/io/sparkycreepster/custom/dataclasses/BloodObserverRegistry.java
@@ -0,0 +1,13 @@
+package io.sparkycreepster.custom.dataclasses;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class BloodObserverRegistry {
+    private static final Map<UUID, PlayerEntity> PlayersVisited = new HashMap<>();
+
+
+}
