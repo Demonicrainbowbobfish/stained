98b5731bc667cdb8b07842c07db91ca4184ba709
= <stinkybob937@gmail.com>
2026-09-08 14:55:11 -0700

    some small changes

--- src/main/java/io/sparkycreepster/custom/items/Ledger.java
+++ src/main/java/io/sparkycreepster/custom/items/Ledger.java
@@ -7,10 +7,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Ledger extends Item {
@@ -36,6 +38,10 @@ public class Ledger extends Item {
                         1.0F
                 );
             }
+        }
+        else {
+            List<LedgerData.Entry> l = LedgerData.getEntries(stack);
+            user.sendMessage(Text.literal(l.toString()));
         }
         return TypedActionResult.success(stack);
     }
